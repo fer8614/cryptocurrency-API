b9da2d6dc9b38eac04b3e534cccdb01e62f17fe6
fer8614 <fernando.inversionmf@gmail.com>
2024-07-19 13:14:05 -0700

    Validating the API response with ZOD

--- src/store.ts
+++ src/store.ts
@@ -17,7 +17,8 @@ export const useCryptoStore = create <CryptoStore> () ( devtools ( ( set ) => ({
     },
 
     fetchData : async ( pair ) => {
-        fetchCurrentCryptoPrice( pair )
+        const result =  await fetchCurrentCryptoPrice( pair )
+        console.log( result );
     }
 
 
