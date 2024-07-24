7a56ca00306fb19c60e7b384eb9d36b0c76dbeb8
fer8614 <fernando.inversionmf@gmail.com>
2024-07-24 13:57:13 -0700

    Adding CSS and loading spinner

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -1,19 +1,26 @@
 import { useMemo } from "react"
 import { useCryptoStore } from "../store"
+import Spinner from "./Spinner"
 
 export default function CryptoPriceDisplay() {
 
     const result = useCryptoStore( ( state ) => state.result ) 
+    const loading = useCryptoStore( ( state ) => state.loading ) 
     const hasResult = useMemo( () => Object.values( result ).length > 0, [ result ] )
 
   return (
-    <div>
-        { hasResult && (
+    <div className="result-wrapper">
+        { loading ? <Spinner /> : hasResult && (
             <div>
                 <h2>Price</h2>
                 <div className="result">
+                    <img src={ `https://www.cryptocompare.com/${ result.IMAGEURL }` } alt="image of crypto" />
                     <div>
                         <p>The price is: <span>{ result.PRICE }</span></p>
+                        <p>Highest price of the day: <span>{ result.HIGHDAY }</span></p>
+                        <p>Lowest price of the day: <span>{ result.LOWDAY }</span></p>
+                        <p>Price variation in the last 24 hours: <span>{ result.CHANGEPCT24HOUR }</span></p>
+                        <p>Last update: <span>{ result.LASTUPDATE }</span></p>
 
                     </div>
                 </div>
--- src/store.ts
+++ src/store.ts
@@ -6,6 +6,7 @@ import { fetchCurrentCryptoPrice, getCryptos } from './services/CryptoService'
 type CryptoStore = {
     cryptoCurrencies: CryptoCurrency[]
     result: CryptoPrice
+    loading: boolean
     fetchCryptos: () => Promise<void>
     fetchData: ( pair : Pair) => Promise<void>
 }
@@ -13,15 +14,18 @@ type CryptoStore = {
 export const useCryptoStore = create <CryptoStore> () ( devtools ( ( set ) => ({
     cryptoCurrencies : [],
     result: {} as CryptoPrice,
+    loading: false,
     fetchCryptos : async () => {
         const cryptoCurrencies = await getCryptos()
         set(() => ({ cryptoCurrencies }))
     },
 
     fetchData : async ( pair ) => {
+        set ( () => ({ loading: true }))
+
         const result =  await fetchCurrentCryptoPrice( pair )
 
-        set ( () => ({ result }))
+        set ( () => ({ result, loading: false }))
     }
 
 
